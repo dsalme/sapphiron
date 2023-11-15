c2658a9e6a7755f6d7b9ce30ffa0d1f77493f654
David Salmeron <david@brainhi.com>
2023-11-15 21:25:26 +0100

    added if else for puma configuration for docker in local

--- config/puma.rb
+++ config/puma.rb
@@ -23,8 +23,11 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#bind "tcp://0.0.0.0:#{ENV['PORT'] || 3000}"
-port ENV.fetch("PORT") { 3000 }
+if ENV["RAILS_ENV"] == "production"
+  port ENV.fetch("PORT") { 3000 }
+else
+  bind "tcp://0.0.0.0:#{ENV['PORT'] || 3000}"
+end
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
