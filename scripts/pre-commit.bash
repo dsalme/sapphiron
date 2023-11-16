f6c85387e6e4a2f5227a817185c3e267b51fed67
David Salmeron <david@brainhi.com>
2023-11-16 21:05:36 +0100

    added precommit hook setup + instructions + pull request template

---
+++ scripts/pre-commit.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Running pre-commit hook"
+if test $(git rev-parse --abbrev-ref HEAD) = "master" ; then 
+  echo "Cannot commit on master"
+  exit 1
+fi
+if test $(git rev-parse --abbrev-ref HEAD) = "develop" ; then 
+  echo "Cannot commit on develop"
+  exit 1
+fi
+./scripts/run-rubocop.bash
+
+# $? stores exit value of the last command
+if [ $? -ne 0 ]; then
+ echo "Code must be clean before commiting"
+ exit 1
+fi
