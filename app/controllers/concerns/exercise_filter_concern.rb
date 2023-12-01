aeaa8fa31159aa432c944d632fcae9cad8b5421f
David Salmeron <david@brainhi.com>
2023-12-01 11:34:53 +0100

    further refining for filtering

--- app/controllers/concerns/exercise_filter_concern.rb
+++ app/controllers/concerns/exercise_filter_concern.rb
@@ -7,10 +7,12 @@ module ExerciseFilterConcern
   included do
   end
   FILTER_PARAMS = {
-    tool_id: { model: Tool, association: :tools, field: :name },
-    muscle_id: { model: Muscle, association: :muscles, field: :name },
+    tool_id: { model: Tool, association: :tools, field: :id },
+    tool_name: { model: Tool, association: :tools, field: :name },
+    muscle_id: { model: Muscle, association: :muscles, field: :id },
     muscle_name: { model: Muscle, association: :muscles, field: :name },
     muscle_group_id: { model: Muscle, association: :muscles, field: :muscle_group_id },
-    movement_pattern_id: { model: MovementPattern, association: :movement_patterns, field: :name }
+    movement_pattern_id: { model: MovementPattern, association: :movement_patterns, field: :id },
+    movement_pattern_name: { model: MovementPattern, association: :movement_patterns, field: :name }
   }.freeze
 end
